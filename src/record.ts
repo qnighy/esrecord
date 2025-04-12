adba1291bd245db998280b388c7df193dcb79515
Masaki Hara <ackie.h.gmai@gmail.com>
2025-04-12 19:18:47 +0900

    Fix isolatedDeclarations

--- src/record.ts
+++ src/record.ts
@@ -18,7 +18,7 @@ export function Record(arg: Record<string, unknown>): Record<string, unknown> {
   return recordInterner.intern(key, () => createPrimitiveRecord(entries));
 }
 
-Record.prototype = null;
+(Record as any).prototype = null;
 
 function createPrimitiveRecord(entries: [string, unknown][]): Record<string, unknown> {
   const rec: Record<string, unknown> = Object.create(null);
