34f24d81b9c698dc9d161608db8fd45e286ddfad
Masaki Hara <ackie.h.gmai@gmail.com>
2025-04-13 14:31:16 +0900

    Rework Tuple types

--- src/tuple.ts
+++ src/tuple.ts
@@ -1,13 +1,47 @@
 import { Interner } from "./interner.ts";
-import { isObject, primitiveTuples, type AnyESTuple } from "./primitives.ts";
+import { isObject, primitiveTuples, type AnyESTuple, type ESTuple } from "./primitives.ts";
 
-const tupleInterner = new Interner<readonly unknown[], readonly unknown[]>();
+const tupleInterner = new Interner<readonly unknown[], AnyESTuple>();
 /**
  * Represents the tuple object's [[TupleData]] internal slot.
  */
-const tupleDataRef = new WeakMap<readonly unknown[], readonly unknown[]>();
+const tupleDataRef = new WeakMap<object, AnyESTuple>();
 
-export function Tuple(...items: readonly unknown[]): readonly unknown[] {
+/**
+ * The type of the {@link Tuple} constructor.
+ */
+export interface TupleConstructor {
+  /**
+   * Creates a new primitive Tuple value from the given items.
+   *
+   * @param items the list of items to create the Tuple from.
+   */
+  <A extends any[]>(...items: A): ESTuple<A>;
+
+  /**
+   * Creates a new primitive Tuple value from the given array or iterable.
+   */
+  from<A extends any[]>(items: A): ESTuple<A>;
+
+  /**
+   * Creates a new primitive Tuple value from the given array or iterable.
+   */
+  from<T>(items: Iterable<T> | ArrayLike<T>): ESTuple<T[]>;
+
+  /**
+   * Creates a new primitive Tuple value from the given array or iterable.
+   */
+  from<T, U>(items: Iterable<T> | ArrayLike<T>, mapfn: (v: T, k: number) => U, thisArg?: any): ESTuple<U[]>;
+
+  /**
+   * Creates a new primitive Tuple value from the given items.
+   *
+   * @param items the list of items to create the Tuple from.
+   */
+  of<A extends any[]>(...items: A): ESTuple<A>;
+}
+
+export const Tuple: TupleConstructor = function Tuple<A extends any[]>(...items: A): ESTuple<A> {
   if (new.target != null) {
     throw new TypeError("Tuple is not a constructor");
   }
@@ -16,8 +50,8 @@ export function Tuple(...items: readonly unknown[]): readonly unknown[] {
       throw new TypeError("Tuple cannot contain object values");
     }
   }
-  return tupleInterner.intern(items, () => createPrimitiveTuple(items));
-}
+  return tupleInterner.intern(items, () => createPrimitiveTuple(items)) as ESTuple<A>;
+} as TupleConstructor;
 
 const tuplePrototype = Object.create(null);
 Object.defineProperty(Tuple, "prototype", {
@@ -27,21 +61,21 @@ Object.defineProperty(Tuple, "prototype", {
   configurable: false,
 });
 
-function createPrimitiveTuple(items: readonly unknown[]): readonly unknown[] {
+function createPrimitiveTuple(items: readonly unknown[]): AnyESTuple {
   const tup = createMinArray(items);
   Object.freeze(tup);
   primitiveTuples.add(tup as readonly unknown[] as AnyESTuple);
-  return tup;
+  return tup as readonly unknown[] as AnyESTuple;
 }
 
 /**
  * Convert a Tuple primitive to a Tuple object.
  */
-export function TupleToObject(tuple: readonly unknown[]): readonly unknown[] {
+export function TupleToObject<A extends any[]>(tuple: ESTuple<A>): Readonly<A> {
   const obj = createMinArray(tuple);
   tupleDataRef.set(obj, tuple);
   Object.freeze(obj);
-  return obj;
+  return obj as readonly unknown[] as Readonly<A>;
 }
 
 /**
@@ -63,6 +97,6 @@ function createMinArray(items: readonly unknown[]): unknown[] {
  * Checks if the given object is a Tuple wrapper object,
  * and if so, returns the Tuple primitive it wraps.
  */
-export function getTuplePrimitive(maybeTuple: unknown): readonly unknown[] | undefined {
-  return tupleDataRef.get(maybeTuple as readonly unknown[]);
+export function getTuplePrimitive(maybeTuple: unknown): AnyESTuple | undefined {
+  return tupleDataRef.get(maybeTuple as object);
 }
